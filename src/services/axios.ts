a7db6da748c8b12663d141fd60ddf6de3ebc5d74
Sliden101 <chessplayz11@gmail.com>
2025-09-14 18:25:23 +0700

    fix: credentials axios

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -4,6 +4,7 @@ export const BACKEND_URL = (import.meta.env.VITE_BACKEND_URL || "http://localhos
 
 const API = axios.create({
     baseURL: BACKEND_URL,
+    withCredentials: true,
 })
 
 // always attach the token
